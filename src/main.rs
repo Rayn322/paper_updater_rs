691dd56e5d536e4c28b8128f2b7bbfaaffeb0233
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-02-08 16:12:26 -0800

    Finish file downloading

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,88 @@
 mod paper_api;
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
 use rfd::FileDialog;
-use std::path::PathBuf;
+use std::{cmp::min, error::Error, fs::File, io::Write, path::PathBuf};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), ()> {
     let file = FileDialog::new()
         .add_filter("Paper Jar (*.jar)", &["jar"])
         .pick_file();
 
     match file {
         Some(file) => {
-            update_server(file);
+            update_server(file).await.expect("Failed to update server!");
         }
         None => println!("No file selected!"),
     }
+
+    Ok(())
 }
 
-fn update_server(file: PathBuf) -> Result<(), ()> {
-    let url = match paper_api::fetch_latest_download() {
-        Ok(url) => url,
+async fn update_server(mut file: PathBuf) -> Result<(), ()> {
+    let download = match paper_api::fetch_latest_download().await {
+        Ok(download) => download,
         Err(_) => return Err(()),
     };
+
+    match std::fs::remove_file(&file) {
+        Ok(_) => {
+            file.pop();
+            file.push(format!("paper-{}-{}.jar", download.version, download.build));
+
+            match file.to_str() {
+                Some(file) => match download_file(&download.url, file).await {
+                    Ok(_) => {}
+                    Err(_) => {
+                        return Err(());
+                    }
+                },
+                None => {
+                    return Err(());
+                }
+            }
+        }
+        Err(_) => {
+            return Err(());
+        }
+    }
+
+    Ok(())
+}
+
+// adapted from https://gist.github.com/giuliano-oliveira/4d11d6b3bb003dba3a1b53f43d81b30d
+async fn download_file(url: &str, path: &str) -> Result<(), Box<dyn Error>> {
+    println!("Downloading {} to {}", url, path);
+
+    let res = reqwest::get(url).await?;
+    let total_size = res.content_length().ok_or("Couldn't get content length")?;
+
+    let bar = ProgressBar::new(total_size);
+    let mut style = match ProgressStyle::default_bar()
+    .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})") {
+        Ok(style) => style,
+        Err(_) => return Err("Error while creating progress bar".into()),
+    };
+
+    style = style.progress_chars("#>-");
+    bar.set_style(style);
+    bar.set_message(format!("Downloading {}", url));
+
+    let mut file = File::create(path)?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err("Error while downloading file"))?;
+        file.write_all(&chunk)
+            .or(Err("Error while writing to file"))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        bar.set_position(new);
+    }
+
+    bar.finish_with_message("Downloaded file successfully!");
 
     Ok(())
 }
--- src/paper_api.rs
+++ src/paper_api.rs
@@ -10,8 +10,14 @@ struct BuildList {
     builds: Vec<i16>,
 }
 
-pub fn fetch_latest_download() -> Result<String, ()> {
-    let version_list = match get_version_list() {
+pub struct Download {
+    pub version: String,
+    pub build: i16,
+    pub url: String,
+}
+
+pub async fn fetch_latest_download() -> Result<Download, ()> {
+    let version_list = match get_version_list().await {
         Ok(version_list) => version_list,
         Err(_) => return Err(()),
     };
@@ -21,7 +27,7 @@ pub fn fetch_latest_download() -> Result<String, ()> {
         None => return Err(()),
     };
 
-    let build_list = match get_build_list(latest_version) {
+    let build_list = match get_build_list(latest_version).await {
         Ok(build_list) => build_list,
         Err(_) => return Err(()),
     };
@@ -31,29 +37,35 @@ pub fn fetch_latest_download() -> Result<String, ()> {
         None => return Err(()),
     };
 
-    println!("version: {0}, build: {1}", latest_version, latest_build);
-
     let download_url = format!(
         "https://papermc.io/api/v2/projects/paper/versions/{0}/builds/{1}/downloads/paper-{0}-{1}.jar",
         latest_version, latest_build
     );
 
-    Ok(download_url)
+    Ok(Download {
+        version: latest_version.to_string(),
+        build: *latest_build,
+        url: download_url,
+    })
 }
 
-fn get_version_list() -> reqwest::Result<VersionList> {
-    let version_list: VersionList =
-        reqwest::blocking::get("https://papermc.io/api/v2/projects/paper")?.json()?;
+async fn get_version_list() -> reqwest::Result<VersionList> {
+    let version_list: VersionList = reqwest::get("https://papermc.io/api/v2/projects/paper")
+        .await?
+        .json()
+        .await?;
 
     Ok(version_list)
 }
 
-fn get_build_list(version: &str) -> reqwest::Result<BuildList> {
-    let build_list: BuildList = reqwest::blocking::get(format!(
+async fn get_build_list(version: &str) -> reqwest::Result<BuildList> {
+    let build_list: BuildList = reqwest::get(format!(
         "https://papermc.io/api/v2/projects/paper/versions/{}",
         version
-    ))?
-    .json()?;
+    ))
+    .await?
+    .json()
+    .await?;
 
     Ok(build_list)
 }
